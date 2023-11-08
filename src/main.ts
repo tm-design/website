038b0ac14acb5314245f55a90bfd24869119f101
Tim Mudd <tm.designartist@gmail.com>
2023-11-08 16:29:43 -0600

    added vuetify to app loading

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
 
 import App from "./App.vue";
 import router from "./router";
@@ -8,5 +17,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(vuetify);
 
 app.mount("#app");
